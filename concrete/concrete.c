e09847998582326625c742587b627d526e0f501a
sam739 <M15755682101@163.com>
2023-02-11 22:48:18 +0800

    小段C语言代码

---
+++ concrete/concrete.c
@@ -0,0 +1,12 @@
+#include  <stdio.h> /*导入库函数stdio.h*/
+int main(void)   /*程序入口，main()函数*/
+{
+      printf("Conrtere contins gravel and cement.\n");
+
+      return 0;
+
+
+
+
+}
+
---
+++ i_dogs/i_dogs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+  int main(void)
+{
+  int dogs;
+
+  printf("How many dog do you have?\n");
+  scanf("%d",&dogs);
+  printf("so you have %d dog(s)!\n",dogs);
+
+
+  return 0;
+
+
+
+
+}
